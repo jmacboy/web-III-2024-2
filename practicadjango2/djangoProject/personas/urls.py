8c578a6f48ad8dbee47edbc5ddb975cfe0559c00
jmacboy <j.m.alvarez.camacho@gmail.com>
2024-08-23 13:29:07 -0400

    actualizado a la clase del 2024-08-20

--- practicadjango2/djangoProject/personas/urls.py
+++ practicadjango2/djangoProject/personas/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("", views.persona_list, name="personas_list"),
     path("create", views.persona_create, name="persona_create"),
     path("store", views.persona_store, name="persona_store"),
+    path("edit/<int:id>", views.persona_edit, name="persona_edit"),
+    path("update/<int:id>", views.persona_update, name="persona_update"),
+    path("delete/<int:id>", views.persona_delete, name="persona_delete"),
 ]
